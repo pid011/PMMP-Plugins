ab566fd781e3cab922b499ea617d2d9fe2c49329
Mohi <0428asd@naver.com>
2015-12-13 15:17:14 +0900

    Update messages.yml, KickAllMain.php

--- KickAll/src/Mohi/KickAll/KickAllMain.php
+++ KickAll/src/Mohi/KickAll/KickAllMain.php
@@ -10,7 +10,12 @@ class kickAllMain extends PluginBase implements Listener {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 	public function onCommand(CommandSender $sender, Command $command, $label, $Array $args) {
-		
+		if(strtolower($command) == $this->get("kor-command") {
+			if(! isset($args[0])) {
+				
+			}
+		}
+		switch
 	}
 	public function kickAll(String $reason) {
 		foreach ( $this->getServer ()->getOnlinePlayers () as $player ) {

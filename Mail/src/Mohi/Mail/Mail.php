f28f9a16b314fcc51c7607eb9d39ed8a567de575
Mohi <0428asd@naver.com>
2015-12-23 08:53:57 +0900

    Updated

---
+++ Mail/src/Mohi/Mail/Mail.php
@@ -0,0 +1,44 @@
+<php?
+namespace Mohi\Mail;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Mail extends PluginBase implements Listner {
+	public function onEnable() {
+		
+	}
+	public function onDisable() {
+		
+	}
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+		if(strtolower($args[0]) == "mail") {
+			if(! isset($args[1])) {
+				$this->alert($sender, "
+				/mail <cheak|list|new>
+				")
+			}
+			switch(strtolower($args[0])) {
+				case "cheak" :
+					
+			}
+		}
+	}
+	public function alert(CommandSender $sender, $message, $prefix = NULL)
+	{
+		if($prefix==NULL){
+			$prefix = "MAIL";
+		}
+		$sender->sendMessage(TextFormat::RED.$prefix." $message");
+	}
+	public function message(CommandSender $sender, $message, $prefix = NULL)
+	{
+		if($prefix==NULL){
+			$prefix = "MAIL"
+		}
+		$sender->sendMessage(TextFormat::DARK_AQUA.$prefix." $message");
+	}
+}
+?>
--- ProtectArea/src/Mohi/ProtectArea/ProtectArea.php
+++ ProtectArea/src/Mohi/ProtectArea/ProtectArea.php
@@ -3,9 +3,16 @@ namespace Mohi\ProtectArea
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class protectArea extends PluginBase implements Listener {
-	
+	public function onEnable() {
+		
+	}
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+		
+	}
 }
 
 
